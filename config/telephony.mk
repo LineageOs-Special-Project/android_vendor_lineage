bf75a6463ea6bdd70d6ac9786c5bba399d930c14
LineageOS-Special-Project <yehonatan@mail.ru>
2024-12-29 18:58:06 +0000

    remove gms conflicts from config files

--- config/telephony.mk
+++ config/telephony.mk
@@ -14,11 +14,7 @@ PRODUCT_PACKAGES += \
 # Telephony packages
 PRODUCT_PACKAGES += \
     Stk
-
-# Default ringtone
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.config.ringtone=Orion.ogg
-
+    
 # Tethering - allow without requiring a provisioning app
 # (for devices that check this)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
