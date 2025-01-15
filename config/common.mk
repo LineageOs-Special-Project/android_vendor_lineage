7cdf2dc15535231d499de6388309c463447036ec
LineageOS-Special-Project <yehonatan@mail.ru>
2025-01-15 00:33:41 +0000

    Rebranding 1.0 - build.props and stuff

--- config/common.mk
+++ config/common.mk
@@ -1,7 +1,7 @@
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
 
-PRODUCT_BRAND ?= LineageOS
+PRODUCT_BRAND ?= LOSP
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
@@ -73,7 +73,7 @@ PRODUCT_COPY_FILES += \
 
 # Enforce privapp-permissions whitelist
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.control_privapp_permissions=enforce
+    ro.control_privapp_permissions=log
 
 ifneq ($(TARGET_DISABLE_LINEAGE_SDK), true)
 # Lineage SDK
@@ -210,10 +210,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     debug.sf.enable_transaction_tracing=false
 endif
 
-# SetupWizard
-PRODUCT_PRODUCT_PROPERTIES += \
-    setupwizard.feature.day_night_mode_enabled=true
-
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/no-rro
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/lineage/overlay/common \
@@ -245,58 +241,89 @@ PRODUCT_PACKAGES += \
     ImmersiveNavigationOverlay 
 
 # Use 2-button navbar by default
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.twobutton
-
-# Enable blur
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.twobutton
+    
+# Blurs
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
     ro.sf.blurs_are_expensive=1 \
     ro.surface_flinger.supports_background_blur=1 \
+    ro.launcher.blur.appLaunch=0
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-dalvik.vm.checkjini=false \
-ro.kernel.android.checkjni=0 \
-ro.kernel.checkjni=0 \
-ro.config.nocheckin=1 \
-debug.systemui.latency_tracking=0 \
-persist.sample.eyetracking.log=0 \
-ro.com.google.locationfeatures=0 \
-ro.com.google.networklocation=0 \
-media.metrics.enabled=0 \
-sys.debug.watchdog=0 \
-logd.statistics=0 \
-media.metrics=0 \
-config.stats=0 \
-persist.sys.loglevel=0 \
-sys.log.app=0 \
-persist.traced.enable=0 \
-logd.statistics=0 \
-persist.sample.eyetracking.log=0 \
-debug.atrace.tags.enableflags=0 \
-debugtool.anrhistory=0 \
-ro.debuggable=1 \
-profiler.debugmonitor=false \
-profiler.launch=false \
-profiler.hung.dumpdobugreport=false \
-trustkernel.log.state=disable \
-debug.mdpcomp.logs=0 \
-debug.atrace.tags.enableflags=0 \
-pm.sleep_mode=1 \
-profiler.force_disable_ulog=true \
-profiler.force_disable_err_rpt=true \
-ro.logd.size.stats=0 \
-debug.atrace.tags.enableflags=0 \
-persist.service.pcsync.enable=0 \
-persist.service.lgospd.enable=0
+    dalvik.vm.checkjini=false \
+    ro.kernel.android.checkjni=0 \
+    ro.kernel.checkjni=0 \
+    ro.config.nocheckin=1 \
+    debug.systemui.latency_tracking=0 \
+    persist.sample.eyetracking.log=0 \
+    ro.com.google.locationfeatures=0 \
+    ro.com.google.networklocation=0 \
+    media.metrics.enabled=0 \
+    sys.debug.watchdog=0 \
+    logd.statistics=0 \
+    media.metrics=0 \
+    config.stats=0 \
+    persist.sys.loglevel=0 \
+    sys.log.app=0 \
+    persist.traced.enable=0 \
+    logd.statistics=0 \
+    persist.sample.eyetracking.log=0 \
+    debug.atrace.tags.enableflags=0 \
+    debugtool.anrhistory=0 \
+    ro.debuggable=1 \
+    profiler.debugmonitor=false \
+    profiler.launch=false \
+    profiler.hung.dumpdobugreport=false \
+    trustkernel.log.state=disable \
+    debug.mdpcomp.logs=0 \
+    debug.atrace.tags.enableflags=0 \
+    pm.sleep_mode=1 \
+    profiler.force_disable_ulog=true \
+    profiler.force_disable_err_rpt=true \
+    ro.logd.size.stats=0 \
+    debug.atrace.tags.enableflags=0 \
+    persist.service.pcsync.enable=0 \
+    persist.service.lgospd.enable=0 \
+    dalvik.vm.debug.alloc=0 \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.error.receiver.system.apps=com.google.android.gms \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
+    ro.setupwizard.rotation_locked=true \
+    ro.com.google.ime.theme_id=5 \
+    ro.opa.eligible_device=true \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    setupwizard.feature.day_night_mode_enabled=true \
+    ro.setupwizard.network_required=false \
+    ro.setupwizard.gservices_delay=-1 \
+    ro.setupwizard.mode=OPTIONAL \
+    setupwizard.feature.predeferred_enabled=false \
+    drm.service.enabled=true \
+    persist.sys.dun.override=0 \
+    persist.sys.disable_rescue=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
     camera.disable_zsl_mode=0 \
     persist.camera.HAL3.enabled=1 \
     persist.camera.ois.disable=0
 
+# Disable touch video heatmap to reduce latency, motion jitter, and CPU usage
+# on supported devices with Deep Press input classifier HALs and models
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.input.video_enabled=false
+
+# Disable async MTE on system_server
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    arm64.memtag.process.system_server=off
+    
+# Enable dex2oat64 to do dexopt
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    dalvik.vm.dex2oat64.enabled=true
+
 include vendor/lineage/config/version.mk
 
 -include vendor/lineage-priv/keys/keys.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
--include vendor/lineage/config/partner_gms.mk
